62163aff8c34da4850b32b01ffe24cbf0d1b0198
Redwanul Karim <karim.redwanul@outlook.com>
2024-07-09 18:46:54 +0200

    Removed intentional bug.

--- project/tests/test_system.py
+++ project/tests/test_system.py
@@ -99,7 +99,7 @@ class TestPipeline(unittest.TestCase):
         conn.close()
         
         # For testing purpose.
-        self.assertEqual(True, False)
+        # self.assertEqual(True, False)
 
         self.assertEqual(len(saved_final_test_data), 10)
         self.assertEqual(len(saved_final_test_data.columns.to_list()), 2)
